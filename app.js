d892442b5537f7446c4436e36fd44efb3d90e1fa
Shoaib Dar <SDAR30@gmail.com>
2021-06-20 12:00:28 -0400

    added delete and put

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const cors = require('cors');
 const transactionsController = require("./controllers/transactions");
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //adds "body" to the text
 app.use(cors());
+
+app.use((req,res,next)=>{
+  next();
+})
 
 app.use("/transactions", transactionsController);
 
 // ROOT
 app.get("/", (req, res) => {
-  res.send("Basic Express App - HOME");
+  res.send("Budgeting APP - BackEnd");
 });
 
 app.get("/*",(req,res)=>{
-  res.status(404).send("PAGE NOT FOUND!!! !!!")
+  res.status(404).send("ERROR -  WRONG PAGE")
 })
 
 module.exports = app;
--- controllers/transactions.js
+++ controllers/transactions.js
@@ -11,4 +11,31 @@ transactions.get("/:index",(req,res)=>{
     transaction ? res.json(transaction) : res.redirect("/No-page-here")
 })
 
+transactions.post("/new", (req,res)=>{
+  const {body} = req;
+  transactionArray.push(body);
+  const newEntry = transactionArray[transactionArray.length-1];
+  res.json(newEntry);
+})
+
+transactions.delete("/:index",(req,res)=>{
+  const {index} = req.params;
+  if(isNaN(index)){
+    res.json("invalid number")
+    return;
+  }else if(index <0 || index > transactionArray.length-1 ){
+    res.json("index out of bounds")
+    return;
+  }
+  const deletedTransaction = transactionArray.splice(index,1);
+  res.json(deletedTransaction[0])
+})
+
+transactions.put("/:index",(req,res)=>{
+  const {index} = req.params;
+  const {body} = req;
+  transactionArray[index] = body;
+  res.json(transactionArray[index]);
+})
+
 module.exports = transactions; 
